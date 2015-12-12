44447ee537a2a728032f0ddf434a6ccc47e3b16f
Corina <corina.aj@gmail.com>
2015-12-11 19:12:53 -1000

    canvas adjustments with lighter grid

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -9,36 +9,36 @@ var module = (function() {
   var colorSelector = function () {
     swatchColor = this.id;
   };
-  var gridNumber = 800;
+  var gridNumber = 1008;
 
-function coloring (colorArray) {
-  var colorArray = colorArray || [
-  '#FFFFFF',
-  '#E60066',
-  '#FF0000',
-  '#FF3300',
-  '#FF6600',
-  '#FF8000',
-  '#FF9900',
-  '#FFCC00',
-  '#FFE500',
-  '#FFFF00',
-  '#CCFF00',
-  '#99FF00',
-  '#33FF00',
-  '#00CC00',
-  '#00B366',
-  '#009999',
-  '#0066B3',
-  '#0033CC',
-  '#1919B3',
-  '#CC0099',
-  '#990099',
-  '#660099',
-  '#400099',
-  '#330099',
-  '#000000'
-  ];
+  function coloring (colors) {
+    var colorArray = colors || [
+    '#FFFFFF',
+    '#E60066',
+    '#FF0000',
+    '#FF3300',
+    '#FF6600',
+    '#FF8000',
+    '#FF9900',
+    '#FFCC00',
+    '#FFE500',
+    '#FFFF00',
+    '#CCFF00',
+    '#99FF00',
+    '#33FF00',
+    '#00CC00',
+    '#00B366',
+    '#009999',
+    '#0066B3',
+    '#0033CC',
+    '#1919B3',
+    '#CC0099',
+    '#990099',
+    '#660099',
+    '#400099',
+    '#330099',
+    '#000000'
+    ];
 
   for (var i = 0; i < colorArray.length; i++) {
     var swatch = document.createElement('div');
@@ -49,45 +49,43 @@ function coloring (colorArray) {
     navigation.appendChild(swatch);
   }
 }
-function renderCanvas (grid) {
-  grid = grid || gridNumber;
-  var ppCanvas = document.createElement('div');
-  ppCanvas.classList.add('pp-canvas');
-  pixelPainter.appendChild(ppCanvas);
+  function renderCanvas (grid) {
+    grid = grid || gridNumber;
+    var ppCanvas = document.createElement('div');
+    ppCanvas.classList.add('pp-canvas');
+    pixelPainter.appendChild(ppCanvas);
 
-  for (var i = 0; i < grid; i++) {
-    var square = document.createElement('button');
-    square.classList.add('square');
-    square.addEventListener('click', function(){
-    this.style.backgroundColor = swatchColor;    //qwerty
+    for (var i = 0; i < grid; i++) {
+      var square = document.createElement('div');
+      square.classList.add('square');
+      square.addEventListener('click', function(){
+      this.style.backgroundColor = swatchColor;
+      });
+      ppCanvas.appendChild(square);
+    }
+  }
+  function clearCanvas () {
+    var clear = document.createElement('button');
+    clear.innerHTML = "Clear";
+    clear.classList.add('clear');
+    navigation.appendChild(clear);
+    clear.addEventListener('click', function() {
+    var selectSquare = document.querySelectorAll('.square');
+    for (var i = 0; i < selectSquare.length; i++) {
+      selectSquare[i].style.backgroundColor = '#FFFFFF';
+    }
     });
-    ppCanvas.appendChild(square);
   }
-}
 
-
-function clearCanvas () {
-  var clear = document.createElement('button');
-  clear.innerHTML = "Clear";
-  clear.classList.add('clear');
-  navigation.appendChild(clear);
-  clear.addEventListener('click', function() {
-  var selectSquare = document.querySelectorAll('.square');
-  for (var i = 0; i < selectSquare.length; i++) {
-    selectSquare[i].style.backgroundColor = '#FFFFFF';
+  function eraseCanvas () {
+    var erase = document.createElement('button');
+    erase.innerHTML = "Erase";
+    erase.classList.add('erase');
+    navigation.appendChild(erase);
+    erase.addEventListener('click', function () {
+      swatchColor = '#FFFFFF';
+    });
   }
-  });
-}
-
-function eraseCanvas () {
-  var erase = document.createElement('button');
-  erase.innerHTML = "Erase";
-  erase.classList.add('erase');
-  navigation.appendChild(erase);
-  erase.addEventListener('click', function () {
-    swatchColor = '#FFFFFF';
-  });
-}
 
   return {
     renderCanvas: renderCanvas,
@@ -95,5 +93,5 @@ function eraseCanvas () {
     eraseCanvas: eraseCanvas,
     coloring: coloring
   };
-})();
+})(); //self-invoking function
 
